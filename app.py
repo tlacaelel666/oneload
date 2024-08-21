682dc3459ec10bbc01084aafa6b3b854864e5592
tlacaelel666 <jakocrazykings@gmail.com>
2024-08-20 23:35:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+import yt_dlp
+from flask import Flask, Blueprint, request, send_file, jsonify, render_template
 
 app=Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['DEBUG'] = os.getenv('DEBUG') == 'True'
+app.register_Blueprint('main', __name__)
 
-@app.route('/')
-def home():
-  return app 
-if __name__ == '__main__':
-  app.run()
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+
+@bp.route('/convert', methods=['POST'])
+def convert_url_to_mp3(stre=None, Exception=None):
+    try:
+        url = request.json('url')
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{'key': 'FFmpegExtractAudio',
+                                'preferredcodec': 'mp3',
+                                'preferredquality': '192',
+                                }],
+            'outtmpl': '%(title)s.%(ext)s',
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            mp3_file = ydl.prepare_filename
+            info_dict.replace('.webm', '.mp3').replace('.ma4', '.mp3')
+        return send_file(mp3_file, mimetype='audio/mp3'), 200
+    except Exception as e:
+        return jsonify({"error": stre(e)}), 500
